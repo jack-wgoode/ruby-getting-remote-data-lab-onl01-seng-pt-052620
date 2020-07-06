b4096bbc2de32d9e256e90d5c7ad1ea81e1d317b
Jack Goode <jack.w.goode@icloud.com>
2020-07-06 16:35:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,32 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester 
+  
+  @@all = []
+  
+  def initialize(url)
+    @url = url
+    @@all << self 
+  end
+  
+   def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  
+  
+  def parse_json
+    # get_requester = GetRequester.new(URL)
+    # results = get_requester.parse_json
+    results = self.parse_json
+    results
+  end 
+  
+  
+  
+  
+  end # Write your code here
